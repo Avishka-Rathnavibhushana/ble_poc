cc9024e8667ef52e260a1907410636ee80144339
Avishka-Rathnavibushana <mailavishka@gmail.com>
2023-03-31 07:58:46 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:ble_poc/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BLE POC',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FlutterBlueApp(),
+    );
+  }
+}
